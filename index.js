1093e0d0213fa47be6a83cb191c7dbc9150dcbe4
Ayush <Ayushverma462002@gmail.com>
2022-08-12 00:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const thisYear = new Date().getFullYear()
 const startTimeOfThisYear = new Date(`${thisYear}-01-01T00:00:00+00:00`).getTime()
 const endTimeOfThisYear = new Date(`${thisYear}-12-31T23:59:59+00:00`).getTime()
@@ -11,13 +12,18 @@ function generateProgressBar() {
       '█'.repeat(passedProgressBarIndex) +
       '▁'.repeat(progressBarCapacity - passedProgressBarIndex)
     return `{ ${progressBar} }`
+}*/
+
+function generatechance() {
+    const chance = Math.floor((Math.random() * 100) + 1);
+    return `{ ${chance} }`
 }
 
 const readme = `\
 ### Hi there 👋
-⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
----
-⏰ Updated on ${new Date().toUTCString()}
+
+Chances of you giving this repo a star are ${generatechance()}%
+
 `
 
 console.log(readme)
